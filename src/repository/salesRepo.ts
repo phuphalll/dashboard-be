d415a6580c8aed6c85aadc3ee8400fbe3759b8f8
phupha <phupha@gopomelo.com>
2023-12-11 13:12:28 +0700

    Update json file reader to using utf-8 encode

--- src/repository/salesRepo.ts
+++ src/repository/salesRepo.ts
@@ -2,15 +2,14 @@ import { TYearlyData } from "@/interfaces/sales.interface";
 import fs from "fs";
 
 let cachedData: any = null;
-const dataPath = "src/mock/sales.report.json";
+const dataPath = "./src/mock/sales.report.json";
 
 const getSalesDataList = (): TYearlyData => {
   if (cachedData === null) {
-    cachedData = fs.readFileSync(dataPath);
-    return cachedData;
-  } else {
-    return cachedData;
+    const rawData = fs.readFileSync(dataPath, "utf8");
+    cachedData = JSON.parse(rawData);
   }
+  return cachedData;
 };
 
 const getAllReport = () => {
@@ -24,8 +23,8 @@ const getReportbyYear = ({
   startYear: number;
   endYear: number;
 }) => {
-  const filteredData = getSalesDataList().filter((item) => {
-    const year = parseInt(item.year, 10);
+  const filteredData = getSalesDataList().filter((yearData) => {
+    const year = parseInt(yearData.year, 10);
     return year >= startYear && year <= endYear;
   });
   return filteredData;
